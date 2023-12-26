f556d5d2ac6da1096c615a8cc1d2e30423402798
Крылов Александр <a.krylov@ispsystem.com>
2023-12-26 17:00:47 +0300

    refactor: link part

--- linksControler.js
+++ linksControler.js
@@ -7,34 +7,41 @@
  * mod.thing == 'a thing'; // true
  */
 
+const {isNorm} = require("./utils");
+
+function getTargets(currentRoom, room, id) {
+    if (isNorm(currentRoom['fromID']) && isNorm(currentRoom['toID'])) {
+        // already saved ids
+        const targetFrom = Game.getObjectById(Memory.linkSettings[room][id]['fromID']);
+        const targetTo = Game.getObjectById(Memory.linkSettings[room][id]['toID']);
+
+        return {targetFrom, targetTo};
+    }
+
+    let targetFrom = Game.rooms[room].lookAt(Memory.linkSettings[room][id]['from'][0], Memory.linkSettings[room][id]['from'][1])[0]['structure'];
+    let targetTo = Game.rooms[room].lookAt(Memory.linkSettings[room][id]['to'][0], Memory.linkSettings[room][id]['to'][1])[0]['structure'];
+    Memory.linkSettings[room][id]['fromID'] = targetFrom.id;
+    Memory.linkSettings[room][id]['toID'] = targetTo.id;
+    targetTo = Game.getObjectById(targetTo.id);
+    targetFrom = Game.getObjectById(targetFrom.id);
+    return {targetFrom, targetTo};
+}
+
 module.exports = {
     towerActions:function(){
-        if (Memory.linkSettings !== undefined ) {
-            for (let room in Memory.linkSettings) {
-                if(Memory.linkSettings[room].length > 0){
-                    for(let id in Memory.linkSettings[room]) {
-                        let targetFrom;
-                        let targetTo;
-                        let currentRoom = Memory.linkSettings[room][id];
-    
-                        if (currentRoom['fromID'] !== undefined && currentRoom['toID'] !== undefined) {
-                            targetFrom = Game.getObjectById(Memory.linkSettings[room][id]['fromID']);
-                            targetTo = Game.getObjectById(Memory.linkSettings[room][id]['toID']);
-                        }
-                        //FIXME
-                        else {
-                            targetFrom = Game.rooms[room].lookAt(Memory.linkSettings[room][id]['from'][0], Memory.linkSettings[room][id]['from'][1])[0]['structure'];
-                            if (targetFrom && targetFrom instanceof StructureLink) {
-                                targetTo = Game.rooms[room].lookAt(Memory.linkSettings[room][id]['to'][0], Memory.linkSettings[room][id]['to'][1])[0]['structure'];
-                                if (targetTo && targetTo instanceof StructureLink) {
-                                    Memory.linkSettings[room][id]['fromID'] = targetFrom.id;
-                                    Memory.linkSettings[room][id]['toID'] = targetTo.id;
-                                }
-                            }
-                        }
-                        targetFrom.sendEnergy(targetTo);
-                    }
-                }
+        if (!isNorm(Memory.linkSettings)) {
+            return;
+        }
+
+        for (const room in Memory.linkSettings) {
+            if(Memory.linkSettings[room].length === 0){
+                continue;
+            }
+
+            for(const id in Memory.linkSettings[room]) {
+                const currentRoom = Memory.linkSettings[room][id];
+                const {targetFrom, targetTo} = getTargets(currentRoom, room, id);
+                targetFrom.sendEnergy(targetTo);
             }
         }
     }
